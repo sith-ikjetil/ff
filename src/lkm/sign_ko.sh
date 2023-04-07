0b6f530d01e61645c2b7ef97c9f2794460e2a1ad
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2023-04-07 05:58:47 +0200

    Fixed minor scripts

--- src/lkm/sign_ko.sh
+++ src/lkm/sign_ko.sh
@@ -1,2 +1,21 @@
-echo "Signing $1"
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /root/module-signing/new.MOK.priv /root/module-signing/MOK.der "$1"
+#!/bin/bash
+
+echo "##"
+echo "## Sign Kernel Object"
+echo "##"
+echo "## Usage: sign_ko.sh <./filename.ko>"
+echo "##"
+
+if [[ $# -eq 1 ]] 
+then
+    echo "> signing $1 <"
+    /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /root/module-signing/new.MOK.priv /root/module-signing/MOK.der "$1"
+    if [[ $? -eq 0 ]]
+    then
+        echo "> signing complete <"
+    else
+        echo "> signing error <"
+    fi
+else 
+    echo "> wrong number of arguments <"
+fi
